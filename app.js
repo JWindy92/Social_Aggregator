22b07ec687ecc0919038fdb41e969a772a0adfd3
John Widmaier <johnwid92@gmail.com>
2020-08-02 16:23:54 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const pug = require('pug');
+const mysql = require('mysql');
+const { body, validationResult } = require('express-validator/check');
+const { sanitzeBody } = require('express-validator/filter');
+const session = require('client-sessions');
+const fs = require('fs');
+
+const User = require('./models/user')
+
+const port = 3000;
+// C:\Users\johnw\OneDrive\Documents\Credentials\RSS_Project_creds\mysql_creds.json
+db_creds = JSON.parse(fs.readFileSync('C:\\Users\\johnw\\OneDrive\\Documents\\Credentials\\RSS_Project_creds\\mysql_creds.json'));
+
+let con = mysql.createConnection(db_creds)
+
+con.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to dev_db!')
+    con.query("CREATE DATABASE IF NOT EXISTS dev_db", (err, result) => {
+        if (err) throw err;
+    });
+})
+
+const app = express();
+app.set('view engine', 'pug')
+app.use(express.static(__dirname + "/public"))
+app.use(express.urlencoded())
+app.use(session({
+    cookieName: 'session',
+    secret: 'random_string_goes_here',
+    duration: 30 * 60 * 1000, // active for 30 min
+    activeDuration: 5 * 60 * 1000, // if expiresIn < activeDuration, session extended by activeDuration
+}));
+
+// MIDDLEWARE #######################
+function protect_route(req, res, next) {
+    if (!session.current_user || current_user.is_authenticated == false) {
+        console.log('not logged in, denied')
+        res.redirect('/')
+    } else {
+        next();
+    }
+}
+
+// ROUTES #######################
+app.get('/', (req, res) => {
+    res.render('index', {title: 'Home', message: 'Hello World!', current_user: session.current_user})
+    if (!session.current_user) {
+        console.log("No current user")
+    } else {
+        console.log(session.current_user)
+    }
+});
+
+app.get('/login', (req, res) => {
+    res.render('login', {title: 'Login', message: 'Login Page'})
+});
+
+app.post('/login', (req, res) => {
+    let sql = `SELECT * FROM Users WHERE email = ?`;
+    con.query(sql, [req.body.email], (err, result, fields) => {
+        if (err) throw err;
+        console.log(result[0])
+        u = new User(result[0].username, result[0].email)
+        let logged_in = u.load(result[0], req.body.password)
+        if (logged_in) {
+            console.log('Login successful')
+            session.current_user = u;
+            res.redirect('/');
+        } else {
+            console.log("Login failed")
+            res.redirect('/login');
+        }
+    });
+})
+
+app.get('/register', (req, res) => {
+    res.render('register', {title: 'Sign Up', message: 'Register Page'})
+});
+
+app.post('/register', [
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
+    body('password2').custom((value, {req}) => (value === req.body.password))
+    ], (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            console.log(errors.array())
+            res.render('register', {title: 'Sign Up', message: 'Register Page'})
+        } else {
+            let u = new User(req.body.email)
+            u.setPassword(req.body.password)
+            u.save(con)
+            res.redirect('/')
+        }
+})
+
+app.get('/validate', (req, res) => {
+    res.render('validate')
+});
+
+// PROTECTED ROUTES 
+
+app.get('/test', protect_route, (req, res) => {
+    res.send('You made it to test!!')
+})
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
---
+++ models/user.js
@@ -0,0 +1,51 @@
+const crypto = require('crypto');
+
+class User {
+ 
+    constructor(email) {
+        this.username = email.substr(0, email.indexOf('@'))
+        this.email = email;
+        this.is_authenticated = false;
+    }
+
+    save(con) {
+        let sql = `INSERT INTO Users (username, email, password_hash, salt, is_authenticated) VALUES (?, ?, ?, ?, ?)`
+        let values = [
+            this.username,
+            this.email,
+            this.password_hash,
+            this.salt,
+            this.is_authenticated,
+        ]
+        con.query(sql, values, (err, result) => {
+            if (err) throw err;
+            console.log("# rows affected: " + result.affectedRows)
+        })
+    }
+
+    load(db_result, password) {
+        if (this.checkPassword(password, db_result.salt, db_result.password_hash)) {
+            console.log("Logging in!!")
+            console.log(db_result)
+            this.username = db_result.username;
+            this.salt = db_result.salt;
+            this.password_hash = db_result.password_hash
+            return true
+        } else {
+            console.log("Invalid username or password!")
+            return false
+        }
+    }
+
+    setPassword(password) {
+        this.salt = crypto.randomBytes(16).toString('hex');
+        this.password_hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`).toString(`hex`);
+    }
+
+    checkPassword(password, salt, password_hash) {
+        let hash = crypto.pbkdf2Sync(password, salt, 1000, 64, `sha512`).toString(`hex`);
+        return password_hash === hash;
+    }
+}
+
+module.exports = User
